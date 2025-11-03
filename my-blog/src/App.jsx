9862eff0fe1a73b20565ea201b6f7961aec4ae6f
bertinmick69 <bertinmick@yahoo.fr>
2025-11-03 15:21:31 +0100

    first commits

---
+++ my-blog/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Bienvenue sur mon blog</h1>
+        <p>Texte</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
